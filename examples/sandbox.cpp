2e2f851ceffdbeca291f988fb84e5b91dfb7afed
de-tolkac <mr.freeman.denis@ya.ru>
2021-08-08 15:33:26 +0300

    Moved main.cpp to examples folder

---
+++ examples/sandbox.cpp
@@ -0,0 +1,57 @@
+#include <tinvest/OpenAPI.h>
+
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::string token;
+    
+    std::ifstream file("token.txt");
+    file >> token;
+    file.close();
+    
+    SandboxRestClient sandboxRest(token);
+
+    // Регистрация клиента в sandbox
+    auto [account, err] = sandboxRest.Register(BrokerAccountType::Tinkoff);
+    if (err) {
+        std::cout << "Error: " << err.message << std::endl;
+    } else {
+        std::cout << "Account id: " << account.brokerAccountId << std::endl;
+    }
+
+
+    // Выполнение регистрации без инициализации объекта ошибки (не стоит так делать)
+    SandboxAccount account2 = sandboxRest.Register(BrokerAccountType::TinkoffIis);
+    std::cout << "Account id: " << account2.brokerAccountId << std::endl;
+
+    
+    // Выставление баланса по валютным позициям
+    err = sandboxRest.SetCurrencyBalance(account.brokerAccountId, Currency::RUB, 250000);
+    if (err) {
+        std::cout << "Error: " << err.message << std::endl;
+    }
+
+
+    // Выставление баланса по инструментальным позициям
+    err = sandboxRest.SetPositionsBalance(account.brokerAccountId, "BBG004730N88", 10000);
+    if (err) {
+        std::cout << "Error: " << err.message << std::endl;
+    }
+
+
+    // Удаление всех позиций
+    err = sandboxRest.Clear(account.brokerAccountId);
+    if (err) {
+        std::cout << "Error: " << err.message << std::endl;
+    }
+
+
+    // Удаление счета
+    err = sandboxRest.Remove(account.brokerAccountId);
+    if (err) {
+        std::cout << "Error: " << err.message << std::endl;
+    }
+
+    return 0;
+}
