685d8d9a70a50e5a55715e39d76e5f1b4facf01f
de-tolkac <mr.freeman.denis@ya.ru>
2021-08-10 13:42:46 +0300

    Implemented toString for custom classes

--- src/utils.cpp
+++ src/utils.cpp
@@ -41,7 +41,73 @@ std::string toString(BrokerAccountType& t) {
     return "TinkoffIis";
 }
 
-BrokerAccountType toBrokerAccountType(std::string s) {
+std::string toString(OrderStatus& s) {
+    switch (s) {
+    case OrderStatus::New:
+        return "New";
+        break;
+    case OrderStatus::PartiallyFill:
+        return "PartiallyFill";
+        break;
+    case OrderStatus::Fill:
+        return "Fill";
+        break;
+    case OrderStatus::Cancelled:
+        return "Cancelled";
+        break;
+    case OrderStatus::Replaced:
+        return "Replaced";
+        break;
+    case OrderStatus::PendingCancel:
+        return "PendingCancel";
+        break;
+    case OrderStatus::Rejected:
+        return "Rejected";
+        break;
+    case OrderStatus::PendingReplace:
+        return "PendingReplace";
+        break;
+    case OrderStatus::PendingNew:
+        return "PendingNew";
+        break;
+    default:
+        return "Error";
+    }
+}
+
+std::string toString(OperationType& t) {
+    if (t == OperationType::Buy) {
+        return "Buy";
+    }
+
+    return "Sell";
+}
+
+OrderStatus toOrderStatus(std::string&& s) {
+    if (s == "New") {
+        return OrderStatus::New;
+    } else if (s == "PartiallyFill") {
+        return OrderStatus::PartiallyFill;
+    } else if (s == "Fill") {
+        return OrderStatus::Fill;
+    } else  if (s == "Cancelled") {
+        return OrderStatus::Cancelled;
+    } else if (s == "Replaced") {
+        return OrderStatus::Replaced;
+    } else if (s == "PendingCancel") {
+        return OrderStatus::PendingCancel;
+    } else if (s == "Rejected") {
+        return OrderStatus::Rejected;
+    } else if (s == "PendingReplace") {
+        return OrderStatus::PendingReplace;
+    } else if (s == "PendingNew") {
+        return OrderStatus::PendingNew;
+    }
+    
+    throw "Invalid order status";    
+}
+
+BrokerAccountType toBrokerAccountType(std::string&& s) {
     if (s == "Tinkoff") {
         return BrokerAccountType::Tinkoff;
     }
--- src/utils.h
+++ src/utils.h
@@ -2,9 +2,16 @@
 
 #include <Currency.h>
 #include <BrokerAccountType.h>
+#include <OrderStatus.h>
+#include <OperationType.h>
 
 #include <string>
 
 std::string toString(Currency&);
 std::string toString(BrokerAccountType&);
-BrokerAccountType toBrokerAccountType(std::string);
+std::string toString(OrderStatus&);
+std::string toString(OperationType&);
+
+OrderStatus toOrderStatus(std::string&&);
+
+BrokerAccountType toBrokerAccountType(std::string&&);
