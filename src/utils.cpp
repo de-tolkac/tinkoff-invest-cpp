b4c754193a2346141bf6013ae0170be50f6589bd
de-tolkac <mr.freeman.denis@ya.ru>
2021-08-12 14:04:12 +0300

    Added to/from string method in utils package

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,6 +1,6 @@
 #include <utils.h>
 
-std::string toString(Currency& c) {
+std::string toString(const Currency& c) {
     switch (c) {
     case Currency::RUB:
         return "RUB";
@@ -29,12 +29,10 @@ std::string toString(Currency& c) {
     case Currency::TRY:
         return "TRY";
         break;
-    default:
-        return "Error";
     }
 }
 
-std::string toString(BrokerAccountType& t) {
+std::string toString(const BrokerAccountType& t) {
     if (t == BrokerAccountType::Tinkoff) {
         return "Tinkoff";
     }
@@ -70,8 +68,6 @@ std::string toString(const OrderStatus& s) {
     case OrderStatus::PendingNew:
         return "PendingNew";
         break;
-    default:
-        return "Error";
     }
 }
 
@@ -89,6 +85,70 @@ std::string toString(const OrderType& t) {
     }
 
     return "Market";
+}
+
+std::string toString(const CandleInterval& i) {
+    switch (i) {
+    case CandleInterval::_1min:
+        return "1min";
+        break;
+    case CandleInterval::_2min:
+        return "2min";
+        break;
+    case CandleInterval::_3min:
+        return "3min";
+        break;
+    case CandleInterval::_5min:
+        return "5min";
+        break;
+    case CandleInterval::_10min:
+        return "10min";
+        break;
+    case CandleInterval::_15min:
+        return "15min";
+        break;
+    case CandleInterval::_30min:
+        return "30min";
+        break;
+    case CandleInterval::_hour:
+        return "hour";
+        break;
+    case CandleInterval::_day:
+        return "day";
+        break;
+    case CandleInterval::_week:
+        return "week";
+        break;
+    case CandleInterval::_month:
+        return "month";
+        break;
+    }
+}
+
+Currency toCurrency(std::string&& c) {
+    if (c == "RUB") {
+        return Currency::RUB;
+    } else if (c == "USD") {
+        return Currency::USD;
+    } else if (c == "EUR") {
+        return Currency::EUR;
+    } else if (c == "GBP") {
+        return Currency::GBP;
+    } else if (c == "HKD") {
+        return Currency::HKD;
+    } else if (c == "CHF") {
+        return Currency::CHF;
+    } else if (c == "JPY") {
+        return Currency::JPY;
+    } else if (c == "CNY") {
+        return Currency::CNY;
+    } else if (c == "TRY") {
+        return Currency::TRY;
+    }
+
+    std::string message = "Invalid currency: ";
+    message += c;
+    throw message;
 }
 
 OrderStatus toOrderStatus(std::string&& s) {
@@ -111,8 +171,10 @@ OrderStatus toOrderStatus(std::string&& s) {
     } else if (s == "PendingNew") {
         return OrderStatus::PendingNew;
     }
-    
-    throw "Invalid order status";    
+     
+    std::string message = "Invalid order status: ";
+    message += s;
+    throw message; 
 }
 
 BrokerAccountType toBrokerAccountType(std::string&& s) {
@@ -120,4 +182,34 @@ BrokerAccountType toBrokerAccountType(std::string&& s) {
         return BrokerAccountType::Tinkoff;
     }
     return BrokerAccountType::TinkoffIis;
+}
+
+CandleInterval toCandleInterval(std::string&& i) {
+    if (i == "1min") {
+        return CandleInterval::_1min;
+    } else if (i == "2min") {
+        return CandleInterval::_2min;
+    } else if (i == "3min") {
+        return CandleInterval::_3min;
+    } else if (i == "5min") {
+        return CandleInterval::_5min;
+    } else if (i == "10min") {
+        return CandleInterval::_10min;
+    } else if (i == "15min") {
+        return CandleInterval::_15min;
+    } else if (i == "30min") {
+        return CandleInterval::_30min;
+    } else if (i == "hour") {
+        return CandleInterval::_hour;
+    } else if (i == "day") {
+        return CandleInterval::_day;
+    } else if (i == "week") {
+        return CandleInterval::_week;
+    } else if (i == "month") {
+        return CandleInterval::_month;
+    }
+
+    std::string message = "Invalid Candle Resolution: ";
+    message += i;
+    throw message; 
 }
--- src/utils.h
+++ src/utils.h
@@ -5,15 +5,18 @@
 #include <OrderStatus.h>
 #include <OperationType.h>
 #include <OrderType.h>
+#include <CandleInterval.h>
 
 #include <string>
 
-std::string toString(Currency&);
-std::string toString(BrokerAccountType&);
+std::string toString(const Currency&);
+std::string toString(const BrokerAccountType&);
 std::string toString(const OrderStatus&);
 std::string toString(const OperationType&);
 std::string toString(const OrderType&);
+std::string toString(const CandleInterval&);
 
+Currency toCurrency(std::string&&);
 OrderStatus toOrderStatus(std::string&&);
-
 BrokerAccountType toBrokerAccountType(std::string&&);
+CandleInterval toCandleInterval(std::string&&);
