e88917f12b8a034bc65a9a84339151d3e05855ad
de-tolkac <mr.freeman.denis@ya.ru>
2021-08-13 17:15:46 +0300

    Created functions for JSON parsing (MoneyAmount)

---
+++ src/MoneyAmount.cpp
@@ -0,0 +1,31 @@
+#include <MoneyAmount.h>
+
+#include <utils.h>
+
+#include <string>
+#include <iostream>
+
+void to_json(Json& j, const MoneyAmount& m) {
+    j = Json {
+                {"currency", toString(m.currency)},
+                {"value", std::to_string(m.value)}
+             };
+}
+
+void from_json(const Json& j, MoneyAmount& m) {
+    try {
+        m.currency = toCurrency(j.at("currency"));
+        j.at("value").get_to(m.value);
+    }
+    catch(std::string error) {
+        throw error;
+    }
+    catch(...) {
+        std::string message = "Impossible to parse JSON to MoneyAmount structure";
+        throw message;
+    }
+}
+
+bool operator==(const MoneyAmount& lhs, const MoneyAmount& rhs) {
+    return lhs.currency == rhs.currency && lhs.value == rhs.value;
+}
--- src/MoneyAmount.h
+++ src/MoneyAmount.h
@@ -2,7 +2,16 @@
 
 #include <Currency.h>
 
+#include <nlohmann/json.hpp>
+
+using Json = nlohmann::json;
+
 struct MoneyAmount {
     Currency currency;
     double value;
 };
+
+void to_json(Json&, const MoneyAmount&);
+void from_json(const Json&, MoneyAmount&);
+
+bool operator==(const MoneyAmount&, const MoneyAmount&);
