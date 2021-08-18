a4ab52155deb6039fea5fcd596346a1b6881716d
de-tolkac <mr.freeman.denis@ya.ru>
2021-08-18 13:01:19 +0300

    Improved tests for Candle parsing

--- tests/candle_json_test.cpp
+++ tests/candle_json_test.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 
 #include <Candle.h>
+#include <utils.h>
 
 #include <string>
 
@@ -30,10 +31,7 @@ TEST(json_test, Candle_get) {
     candle1.highPrice = 30.2;
     candle1.lowPrice = 40.3;
     candle1.volume = 50.4;
-    const char* timeBuffer = "2019-08-19T18:38:33+03:00";
-    struct tm time;
-    strptime(timeBuffer, "%Y-%m-%dT%H:%M:%S%c%c%c:%c%c", &time);
-    candle1.time = timegm(&time);
+    candle1.time = toDate("2019-08-19T18:38:33+03:00");
 
     Candle candle2 = j.get<Candle>();
 
@@ -49,10 +47,7 @@ TEST(json_test, Candle_Parse) {
     candle1.highPrice = 30.2;
     candle1.lowPrice = 40.3;
     candle1.volume = 50.4;
-    const char* timeBuffer = "2019-08-19T18:38:33+03:00";
-    struct tm time;
-    strptime(timeBuffer, "%Y-%m-%dT%H:%M:%S%c%c%c:%c%c", &time);
-    candle1.time = timegm(&time);
+    candle1.time = toDate("2019-08-19T18:38:33+03:00");
 
     std::string text = R"(
         {
@@ -69,4 +64,132 @@ TEST(json_test, Candle_Parse) {
     Candle candle2 = Json::parse(text);
 
     ASSERT_EQ(candle1, candle2); 
+}
+
+TEST(json_test, Candle_Exception_figi) {
+    Json j = R"(
+        {
+            "interval": "1min",
+            "o": 10.0,
+            "c": 20.1,
+            "h": 30.2,
+            "l": 40.3,
+            "v": 50.4,
+            "time": "2019-08-19T18:38:33+03:00"
+        }
+    )"_json;
+
+    ASSERT_THROW(j.get<Candle>(), std::string);  
+}
+
+TEST(json_test, Candle_Exception_interval) {
+    Json j = R"(
+        {
+            "figi": "testFigi123456789",
+            "o": 10.0,
+            "c": 20.1,
+            "h": 30.2,
+            "l": 40.3,
+            "v": 50.4,
+            "time": "2019-08-19T18:38:33+03:00"
+        }
+    )"_json;
+
+    ASSERT_THROW(j.get<Candle>(), std::string);  
+}
+
+TEST(json_test, Candle_Exception_o) {
+    Json j = R"(
+        {
+            "figi": "testFigi123456789",
+            "interval": "1min",
+            "c": 20.1,
+            "h": 30.2,
+            "l": 40.3,
+            "v": 50.4,
+            "time": "2019-08-19T18:38:33+03:00"
+        }
+    )"_json;
+
+    ASSERT_THROW(j.get<Candle>(), std::string);  
+}
+
+TEST(json_test, Candle_Exception_c) {
+    Json j = R"(
+        {
+            "figi": "testFigi123456789",
+            "interval": "1min",
+            "o": 10.0,
+            "h": 30.2,
+            "l": 40.3,
+            "v": 50.4,
+            "time": "2019-08-19T18:38:33+03:00"
+        }
+    )"_json;
+
+    ASSERT_THROW(j.get<Candle>(), std::string);  
+}
+
+TEST(json_test, Candle_Exception_h) {
+    Json j = R"(
+        {
+            "figi": "testFigi123456789",
+            "interval": "1min",
+            "o": 10.0,
+            "c": 20.1,
+            "l": 40.3,
+            "v": 50.4,
+            "time": "2019-08-19T18:38:33+03:00"
+        }
+    )"_json;
+
+    ASSERT_THROW(j.get<Candle>(), std::string);  
+}
+
+TEST(json_test, Candle_Exception_l) {
+    Json j = R"(
+        {
+            "figi": "testFigi123456789",
+            "interval": "1min",
+            "o": 10.0,
+            "c": 20.1,
+            "h": 30.2,
+            "v": 50.4,
+            "time": "2019-08-19T18:38:33+03:00"
+        }
+    )"_json;
+
+    ASSERT_THROW(j.get<Candle>(), std::string);  
+}
+
+TEST(json_test, Candle_Exception_v) {
+    Json j = R"(
+        {
+            "figi": "testFigi123456789",
+            "interval": "1min",
+            "o": 10.0,
+            "c": 20.1,
+            "h": 30.2,
+            "l": 40.3,
+            "time": "2019-08-19T18:38:33+03:00"
+        }
+    )"_json;
+
+    ASSERT_THROW(j.get<Candle>(), std::string);  
+}
+
+TEST(json_test, Candle_Exception_time) {
+    Json j = R"(
+        {
+            "figi": "testFigi123456789",
+            "interval": "1min",
+            "o": 10.0,
+            "c": 20.1,
+            "h": 30.2,
+            "l": 40.3,
+            "v": 50.4
+        }
+    )"_json;
+
+    ASSERT_THROW(j.get<Candle>(), std::string);  
 }
