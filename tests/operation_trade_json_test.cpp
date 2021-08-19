3664d7040293ba93ec37199df50d149c38ceee4d
de-tolkac <mr.freeman.denis@ya.ru>
2021-08-19 13:52:32 +0300

    Improved tests for OperationTrade parsing

--- tests/operation_trade_json_test.cpp
+++ tests/operation_trade_json_test.cpp
@@ -39,3 +39,51 @@ TEST(json_test, OperationTrade_Parse) {
 
     ASSERT_EQ(operationTrade1, operationTrade2);
 }
+
+TEST(json_test, OperationTrade_Exception_tradeId) {
+    Json j = R"(
+            {
+                "date": "2019-08-19T18:38:33+03:00",
+                "price": 1000,
+                "quantity": 200
+            }
+    )"_json;
+
+    ASSERT_THROW(j.get<OperationTrade>(), std::string);
+}
+
+TEST(json_test, OperationTrade_Exception_date) {
+    Json j = R"(
+            {
+                "tradeId": "testTradeId",
+                "price": 1000,
+                "quantity": 200
+            }
+    )"_json;
+
+    ASSERT_THROW(j.get<OperationTrade>(), std::string);
+}
+
+TEST(json_test, OperationTrade_Exception_price) {
+    Json j = R"(
+            {
+                "tradeId": "testTradeId",
+                "date": "2019-08-19T18:38:33+03:00",
+                "quantity": 200
+            }
+    )"_json;
+
+    ASSERT_THROW(j.get<OperationTrade>(), std::string);
+}
+
+TEST(json_test, OperationTrade_Exception_quantity) {
+    Json j = R"(
+            {
+                "tradeId": "testTradeId",
+                "date": "2019-08-19T18:38:33+03:00",
+                "price": 1000
+            }
+    )"_json;
+
+    ASSERT_THROW(j.get<OperationTrade>(), std::string);
+}
