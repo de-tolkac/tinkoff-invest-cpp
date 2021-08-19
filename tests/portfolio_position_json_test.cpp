d639b80007ade0b4d52eb4907c3b8706f6101f4d
de-tolkac <mr.freeman.denis@ya.ru>
2021-08-19 16:51:18 +0300

    Improved tests for PortfolioPosition parsing

--- tests/portfolio_position_json_test.cpp
+++ tests/portfolio_position_json_test.cpp
@@ -93,3 +93,344 @@ TEST(json_test, PortfolioPosition_Parse) {
 
     ASSERT_EQ(position1, position2);
 }
+
+TEST(json_test, PortfolioPosition_Missed_isin) {
+    Json j = R"(
+        {
+            "figi": "testFIGI",
+            "ticker": "ticker-test",
+            "instrumentType": "Stock",
+            "balance": 1000,
+            "blocked": 20,
+            "expectedYield": {
+                "currency": "RUB",
+                "value": 10
+            },
+            "lots": 220,
+            "averagePositionPrice": {
+                "currency": "USD",
+                "value": 20
+            },
+            "averagePositionPriceNoNkd": {
+                "currency": "RUB",
+                "value": 30
+            },
+            "name": "testName"
+        }
+    )"_json;
+
+    PortfolioPosition position1;
+    position1.figi = "testFIGI";
+    position1.ticker = "ticker-test";
+    position1.instrumentType = InstrumentType::Stock;
+    position1.balance = 1000;
+    position1.blocked = 20;
+    position1.lots = 220;
+    position1.name = "testName";
+    position1.expectedYield = {Currency::RUB, 10};
+    position1.averagePositionPrice = {Currency::USD, 20};
+    position1.averagePositionPriceNoNkd = {Currency::RUB, 30};
+
+    PortfolioPosition position2 = j.get<PortfolioPosition>();
+
+    ASSERT_EQ(position1, position2);
+}
+
+TEST(json_test, PortfolioPosition_Missed_blocked) {
+    Json j = R"(
+        {
+            "figi": "testFIGI",
+            "ticker": "ticker-test",
+            "isin": "tets-isin",
+            "instrumentType": "Stock",
+            "balance": 1000,
+            "expectedYield": {
+                "currency": "RUB",
+                "value": 10
+            },
+            "lots": 220,
+            "averagePositionPrice": {
+                "currency": "USD",
+                "value": 20
+            },
+            "averagePositionPriceNoNkd": {
+                "currency": "RUB",
+                "value": 30
+            },
+            "name": "testName"
+        }
+    )"_json;
+
+    PortfolioPosition position1;
+    position1.figi = "testFIGI";
+    position1.ticker = "ticker-test";
+    position1.isin = "tets-isin";
+    position1.instrumentType = InstrumentType::Stock;
+    position1.balance = 1000;
+    position1.lots = 220;
+    position1.name = "testName";
+    position1.expectedYield = {Currency::RUB, 10};
+    position1.averagePositionPrice = {Currency::USD, 20};
+    position1.averagePositionPriceNoNkd = {Currency::RUB, 30};
+
+    PortfolioPosition position2 = j.get<PortfolioPosition>();
+
+    ASSERT_EQ(position1, position2);
+}
+
+TEST(json_test, PortfolioPosition_Missed_expectedYield) {
+    Json j = R"(
+        {
+            "figi": "testFIGI",
+            "ticker": "ticker-test",
+            "isin": "tets-isin",
+            "instrumentType": "Stock",
+            "balance": 1000,
+            "blocked": 20,
+            "lots": 220,
+            "averagePositionPrice": {
+                "currency": "USD",
+                "value": 20
+            },
+            "averagePositionPriceNoNkd": {
+                "currency": "RUB",
+                "value": 30
+            },
+            "name": "testName"
+        }
+    )"_json;
+
+    PortfolioPosition position1;
+    position1.figi = "testFIGI";
+    position1.ticker = "ticker-test";
+    position1.isin = "tets-isin";
+    position1.instrumentType = InstrumentType::Stock;
+    position1.balance = 1000;
+    position1.blocked = 20;
+    position1.lots = 220;
+    position1.name = "testName";
+    position1.averagePositionPrice = {Currency::USD, 20};
+    position1.averagePositionPriceNoNkd = {Currency::RUB, 30};
+
+    PortfolioPosition position2 = j.get<PortfolioPosition>();
+
+    ASSERT_EQ(position1, position2);
+}
+
+TEST(json_test, PortfolioPosition_Missed_averagePositionPrice) {
+    Json j = R"(
+        {
+            "figi": "testFIGI",
+            "ticker": "ticker-test",
+            "isin": "tets-isin",
+            "instrumentType": "Stock",
+            "balance": 1000,
+            "blocked": 20,
+            "expectedYield": {
+                "currency": "RUB",
+                "value": 10
+            },
+            "lots": 220,
+            "averagePositionPriceNoNkd": {
+                "currency": "RUB",
+                "value": 30
+            },
+            "name": "testName"
+        }
+    )"_json;
+
+    PortfolioPosition position1;
+    position1.figi = "testFIGI";
+    position1.ticker = "ticker-test";
+    position1.isin = "tets-isin";
+    position1.instrumentType = InstrumentType::Stock;
+    position1.balance = 1000;
+    position1.blocked = 20;
+    position1.lots = 220;
+    position1.name = "testName";
+    position1.expectedYield = {Currency::RUB, 10};
+    position1.averagePositionPriceNoNkd = {Currency::RUB, 30};
+
+    PortfolioPosition position2 = j.get<PortfolioPosition>();
+
+    ASSERT_EQ(position1, position2);
+}
+
+TEST(json_test, PortfolioPosition_Missed_averagePositionPriceNoNkd) {
+    Json j = R"(
+        {
+            "figi": "testFIGI",
+            "ticker": "ticker-test",
+            "isin": "tets-isin",
+            "instrumentType": "Stock",
+            "balance": 1000,
+            "blocked": 20,
+            "expectedYield": {
+                "currency": "RUB",
+                "value": 10
+            },
+            "lots": 220,
+            "averagePositionPrice": {
+                "currency": "USD",
+                "value": 20
+            },
+            "name": "testName"
+        }
+    )"_json;
+
+    PortfolioPosition position1;
+    position1.figi = "testFIGI";
+    position1.ticker = "ticker-test";
+    position1.isin = "tets-isin";
+    position1.instrumentType = InstrumentType::Stock;
+    position1.balance = 1000;
+    position1.blocked = 20;
+    position1.lots = 220;
+    position1.name = "testName";
+    position1.expectedYield = {Currency::RUB, 10};
+    position1.averagePositionPrice = {Currency::USD, 20};
+
+    PortfolioPosition position2 = j.get<PortfolioPosition>();
+
+    ASSERT_EQ(position1, position2);
+}
+
+TEST(json_test, PortfolioPosition_Exception_figi) {
+    Json j = R"(
+        {
+            "ticker": "ticker-test",
+            "isin": "tets-isin",
+            "instrumentType": "Stock",
+            "balance": 1000,
+            "blocked": 20,
+            "expectedYield": {
+                "currency": "RUB",
+                "value": 10
+            },
+            "lots": 220,
+            "averagePositionPrice": {
+                "currency": "USD",
+                "value": 20
+            },
+            "averagePositionPriceNoNkd": {
+                "currency": "RUB",
+                "value": 30
+            },
+            "name": "testName"
+        }
+    )"_json;
+
+    ASSERT_THROW(j.get<PortfolioPosition>(), std::string);
+}
+
+TEST(json_test, PortfolioPosition_Exception_instrumentType) {
+    Json j = R"(
+        {
+            "figi": "testFIGI",
+            "ticker": "ticker-test",
+            "isin": "tets-isin",
+            "balance": 1000,
+            "blocked": 20,
+            "expectedYield": {
+                "currency": "RUB",
+                "value": 10
+            },
+            "lots": 220,
+            "averagePositionPrice": {
+                "currency": "USD",
+                "value": 20
+            },
+            "averagePositionPriceNoNkd": {
+                "currency": "RUB",
+                "value": 30
+            },
+            "name": "testName"
+        }
+    )"_json;
+
+    ASSERT_THROW(j.get<PortfolioPosition>(), std::string);
+}
+
+TEST(json_test, PortfolioPosition_Exception_balance) {
+    Json j = R"(
+        {
+            "figi": "testFIGI",
+            "ticker": "ticker-test",
+            "isin": "tets-isin",
+            "instrumentType": "Stock",
+            "blocked": 20,
+            "expectedYield": {
+                "currency": "RUB",
+                "value": 10
+            },
+            "lots": 220,
+            "averagePositionPrice": {
+                "currency": "USD",
+                "value": 20
+            },
+            "averagePositionPriceNoNkd": {
+                "currency": "RUB",
+                "value": 30
+            },
+            "name": "testName"
+        }
+    )"_json;
+
+    ASSERT_THROW(j.get<PortfolioPosition>(), std::string);
+}
+
+TEST(json_test, PortfolioPosition_Exception_lots) {
+    Json j = R"(
+        {
+            "figi": "testFIGI",
+            "ticker": "ticker-test",
+            "isin": "tets-isin",
+            "instrumentType": "Stock",
+            "balance": 1000,
+            "blocked": 20,
+            "expectedYield": {
+                "currency": "RUB",
+                "value": 10
+            },
+            "averagePositionPrice": {
+                "currency": "USD",
+                "value": 20
+            },
+            "averagePositionPriceNoNkd": {
+                "currency": "RUB",
+                "value": 30
+            },
+            "name": "testName"
+        }
+    )"_json;
+
+    ASSERT_THROW(j.get<PortfolioPosition>(), std::string);
+}
+
+TEST(json_test, PortfolioPosition_Exception_name) {
+    Json j = R"(
+        {
+            "figi": "testFIGI",
+            "ticker": "ticker-test",
+            "isin": "tets-isin",
+            "instrumentType": "Stock",
+            "balance": 1000,
+            "blocked": 20,
+            "expectedYield": {
+                "currency": "RUB",
+                "value": 10
+            },
+            "lots": 220,
+            "averagePositionPrice": {
+                "currency": "USD",
+                "value": 20
+            },
+            "averagePositionPriceNoNkd": {
+                "currency": "RUB",
+                "value": 30
+            }
+        }
+    )"_json;
+
+    ASSERT_THROW(j.get<PortfolioPosition>(), std::string);
+}
