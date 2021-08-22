1b22fc6ccb1e9b21221ba2077e2f0a8867c64b9a
de-tolkac <mr.freeman.denis@ya.ru>
2021-08-22 16:23:12 +0300

    Converted Currency enum to class

--- src/Currency.h
+++ src/Currency.h
@@ -1,5 +1,72 @@
 #pragma once
 
+#include <string>
+
+/*
 enum class Currency {
     RUB, USD, EUR, GBP, HKD, CHF, JPY, CNY, TRY
+};
+*/
+
+// https://stackoverflow.com/a/53284026/16689699
+
+class Currency {
+public:
+    enum CurrencyType {
+        RUB, USD, EUR, GBP, HKD, CHF, JPY, CNY, TRY
+    };
+    
+    Currency() = default;
+
+    constexpr Currency(CurrencyType type)
+        : value(type)
+    {}
+
+    operator CurrencyType() const {
+        return value; 
+    } 
+
+    explicit operator bool() = delete;
+
+    constexpr bool operator==(Currency currency) const {
+        return value == currency.value; 
+    }
+
+    constexpr bool operator!=(Currency currency) const { 
+        return value != currency.value; 
+    }
+
+    std::string to_string() const {
+        switch (value) {
+        case Currency::RUB:
+            return "RUB";
+            break;
+        case Currency::USD:
+            return "USD";
+            break;
+        case Currency::EUR:
+            return "EUR";
+            break;
+        case Currency::GBP:
+            return "GBP";
+            break;
+        case Currency::HKD:
+            return "HKD";
+            break;
+        case Currency::CHF:
+            return "CHF";
+            break;
+        case Currency::JPY:
+            return "JPY";
+            break;
+        case Currency::CNY:
+            return "CNY";
+            break;
+        case Currency::TRY:
+            return "TRY";
+            break;
+        }
+    }
+private:
+    CurrencyType value;
 };
